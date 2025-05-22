6d3a823bb1a07609315a0ba5028a1f3573920204
Albert M. Canoy <80763419+Canz-143@users.noreply.github.com>
2025-05-22 13:39:14 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/AutoPilotSystems.tsx';
+import HomePage from './pages/index.tsx';
 
 function App() {
   return (
