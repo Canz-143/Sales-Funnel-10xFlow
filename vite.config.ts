909fefb69d2bf3f242d65a93777fdc8594ad510a
Albert M. Canoy <80763419+Canz-143@users.noreply.github.com>
2025-05-22 12:35:15 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // 👈 Tells Vite to resolve from root (important for Vercel)
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react'], // your custom optimization rule
   },
 });
